d87326c50dff4d57a46a8b432503571692251ff9
Minjae Chang <minjaejchang@gmail.com>
2022-06-19 00:38:12 +1200

    Implement filter feature

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ const input = document.querySelector('.input');
 const deleteBtn = document.querySelector('.cross-icon');
 const checkIcon = document.querySelector('.check-icon');
 const todoItem = document.querySelector('.todo-item');
+const filter = document.querySelector('.filter');
+const filterItem = document.querySelector('.filter-item');
 
 function onAdd() {
   const text = input.value;
@@ -53,5 +55,34 @@ todoList.addEventListener('click', (e) => {
     item.classList.toggle('active');
     const todo = item.parentElement.parentElement;
     todo.classList.toggle('complete');
+  }
+});
+
+filter.addEventListener('click', (e) => {
+  // const filterName = e.target.innerText;
+
+  e.target.classList.toggle('active');
+  const todos = todoList.childNodes;
+
+  if (filterName === 'Completed') {
+    todos.forEach((todo) => {
+      if (todo.classList.contains('complete')) {
+        todo.style.display = 'flex';
+      } else {
+        todo.style.display = 'none';
+      }
+    });
+  } else if (filterName === 'Active') {
+    todos.forEach((todo) => {
+      if (!todo.classList.contains('complete')) {
+        todo.style.display = 'flex';
+      } else {
+        todo.style.display = 'none';
+      }
+    });
+  } else if (filterName === 'All') {
+    todos.forEach((todo) => {
+      todo.style.display = 'flex';
+    });
   }
 });
